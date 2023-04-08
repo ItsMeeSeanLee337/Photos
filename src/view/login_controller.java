310978a6ae43011104b54a011fd03731eb3d5e58
Sean Lee <slee206061@gmail.com>
2023-04-08 15:19:49 -0400

    First implementation of login method

--- src/view/login_controller.java
+++ src/view/login_controller.java
@@ -1,10 +1,17 @@
 package view;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class login_controller {
+public class login_controller 
+{
 
     @FXML
     private Button LoginButton;
@@ -12,4 +19,47 @@ public class login_controller {
     @FXML
     private TextField UsernameField;
 
+    @FXML
+    private void login() 
+    {
+        String username = UsernameField.getText();
+        
+        if (username.equals("admin")) 
+        {
+            // Switch to the admin view
+            try 
+            {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Admin.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) LoginButton.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show();
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        } 
+        else if (username.equals("stock")) 
+        {
+            // Switch to the stock album view
+            try 
+            {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("Album View.fxml"));
+                Parent root = loader.load();
+                Scene scene = new Scene(root);
+                Stage stage = (Stage) LoginButton.getScene().getWindow();
+                stage.setScene(scene);
+                stage.show(); // TODO: Current implementation of this method just goes into the album view if it is stock or a user, need to find a way to decide which album to go into
+            } 
+            catch (IOException e) 
+            {
+                e.printStackTrace();
+            }
+        } 
+        else 
+        { // Go to the user's album
+        }
+    }
 }
