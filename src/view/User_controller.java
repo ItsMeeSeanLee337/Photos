961724e86cb1fdc465b74031a1d3ad4be1cd524f
Sean Lee <slee206061@gmail.com>
2023-04-08 16:40:00 -0400

    Created TODO list for all methods in User_Controller

--- src/view/User_controller.java
+++ src/view/User_controller.java
@@ -28,4 +28,34 @@ public class User_controller {
     @FXML
     private Button RenameAlbumButton;
 
+    @FXML
+    private void createAlbum()
+    {
+        // TODO: Create album, name of album will be whatever is in NameField
+    }
+
+    @FXML
+    private void DeleteAlbum()
+    {
+        // TODO: Delete album, album to be deleted is the one selected by the user in the choice box
+    }
+
+    @FXML
+    private void Logout()
+    {
+        // TODO: Logout from the current user
+    }
+
+    @FXML
+    private void OpenAlbum()
+    {
+        // TODO: Open album, album to be opened is the one selected by the user in the choice box
+    }
+
+    @FXML
+    private void RenameAlbum()
+    {
+        // TODO: Rename the album, the album selected in the choice box will be renamed into whatever is currently in NameField
+    }
+
 }
