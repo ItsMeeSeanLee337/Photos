542f460525e5ca2c4af11ea9cdc4b7c2bf658c74
Sean Lee <slee206061@gmail.com>
2023-04-08 15:32:51 -0400

    PhotoApp class for storing username, album, and photo data

---
+++ src/view/PhotoApp.java
@@ -0,0 +1,98 @@
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class PhotoApp implements Serializable {
+    
+    private static final long serialVersionUID = 1L; // for serialization
+    
+    private HashMap<String, User> users; // map of usernames to User objects
+    
+    public PhotoApp() {
+        users = new HashMap<>();
+    }
+    
+    public void addUser(String username) {
+        User user = new User(username);
+        users.put(username, user);
+    }
+    
+    public User getUser(String username) {
+        return users.get(username);
+    }
+    
+    public class User implements Serializable {
+        
+        private static final long serialVersionUID = 1L; // for serialization
+        
+        private String username;
+        private HashMap<String, Album> albums;
+        
+        public User(String username) {
+            this.username = username;
+            albums = new HashMap<>();
+        }
+        
+        public void addAlbum(String albumName) {
+            Album album = new Album(albumName);
+            albums.put(albumName, album);
+        }
+        
+        public Album getAlbum(String albumName) {
+            return albums.get(albumName);
+        }
+        
+        public ArrayList<Album> getAlbums() {
+            return new ArrayList<>(albums.values());
+        }
+        
+        public void removeAlbum(String albumName) {
+            albums.remove(albumName);
+        }
+    }
+    
+    public class Album implements Serializable {
+        
+        private static final long serialVersionUID = 1L; // for serialization
+        
+        private String name;
+        private HashMap<String, Photo> photos;
+        
+        public Album(String name) {
+            this.name = name;
+            photos = new HashMap<>();
+        }
+        
+        public void addPhoto(String photoName) {
+            Photo photo = new Photo(photoName);
+            photos.put(photoName, photo);
+        }
+        
+        public ArrayList<Photo> getPhotos() {
+            return new ArrayList<>(photos.values());
+        }
+        
+        public void removePhoto(String photoName) {
+            photos.remove(photoName);
+        }
+        
+        public String getName() {
+            return name;
+        }
+    }
+    
+    public class Photo implements Serializable {
+        
+        private static final long serialVersionUID = 1L; // for serialization
+        
+        private String name;
+        
+        public Photo(String name) {
+            this.name = name;
+        }
+        
+        public String getName() {
+            return name;
+        }
+    }
+}
