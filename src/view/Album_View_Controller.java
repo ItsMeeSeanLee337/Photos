452f349d805d1549419a49debe069c85a0f2ffda
Sean Lee <slee206061@gmail.com>
2023-04-08 16:30:50 -0400

    Implemented TODO list for Album_View_Controller methods

--- src/view/Album_View_Controller.java
+++ src/view/Album_View_Controller.java
@@ -33,4 +33,54 @@ public class Album_View_Controller {
     @FXML
     private Button RenamePhotoButton;
 
+    @FXML
+    private void AddNewPhoto()
+    {
+        // TODO: Adds a new photo to the current album
+    }
+
+    @FXML
+    private void AddTags()
+    {
+        // TODO: Adds tag(s) to the current photo in the image view
+        // BUG: Currently there is no way to input a tag to add or remove, need to fix ASAP
+    }
+
+    @FXML
+    private void DeletePhoto()
+    {
+        // TODO: Deletes the current photo from the album
+    }
+
+    @FXML
+    private void GoToDetailView()
+    {
+        // TODO: Goes to a detailed photo view of the current photo
+    }
+
+    @FXML
+    private void nextPhoto()
+    {
+        // TODO: Goes to the next photo
+    }
+
+    @FXML
+    private void previousPhoto()
+    {
+        // TODO: Goes to the previous photo
+    }
+
+    @FXML
+    private void RemoveTags()
+    {
+        // TODO: Removes tag(s) from current photo
+        // BUG: Currently there is no way to input a tag to add or remove, need to fix ASAP
+    }
+
+    @FXML
+    private void RenamePhoto()
+    {
+        // TODO: Rename current phot
+        // BUG: Currently there is no field or way to rename a photo, need to fix ASAP
+    }
 }
